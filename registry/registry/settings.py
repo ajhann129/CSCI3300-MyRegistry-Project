5a46687fe561f7d40e6ac4aa959505a45b344cfd
George <lwhittington2231@gmail.com>
2024-03-21 13:11:46 -0400

    Created project wide static folder and added background.jpg to it

--- registry/registry/settings.py
+++ registry/registry/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
+    BASE_DIR / 'static',
     os.path.join(BASE_DIR, 'account_app/static'),
     os.path.join(BASE_DIR, 'registry_app/static'),
 ]
