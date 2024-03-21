bd1051aca8ffec74d7a276ef3f6b4c54b3ea708d
George <lwhittington2231@gmail.com>
2024-03-21 10:25:49 -0400

    Updated settings.py and urls.py files with static

--- registry/registry/urls.py
+++ registry/registry/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account_app.urls')),
     path('home/', include('registry_app.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
