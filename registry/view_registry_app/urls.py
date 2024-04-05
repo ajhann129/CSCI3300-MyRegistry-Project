bc4ae3944482f5bb8fca1df2bea7d4cce93e42da
George <lwhittington2231@gmail.com>
2024-04-04 21:14:29 -0400

    Create the create_item view and html page

--- registry/view_registry_app/urls.py
+++ registry/view_registry_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'view_registry_app'
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('create_item/', views.create_item, name='create_item'),
 ]
--- registry/view_registry_app/views.py
+++ registry/view_registry_app/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
+
+def create_item(request, wishlist_id):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.wishlist_id = wishlist_id
+            item.save()
+            return redirect('view_registry_app:dashboard')  # Redirect to the dashboard
+    else:
+        form = ItemForm()
+    return render(request, 'view_registry_app/create_item.html', {'form': form})
 
 def dashboard(request):
     return render(request, 'view_registry_app/dashboard.html')
