829b1d4eaa69af0176f24f6a521c03de76b81a53
George <lwhittington2231@gmail.com>
2024-04-11 23:42:20 -0400

    Added option url (link) text field to add to items and restructured how the items are displayed

--- registry/view_registry_app/models.py
+++ registry/view_registry_app/models.py
@@ -12,6 +12,7 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
     description = models.TextField(blank=True)
+    link = models.URLField(blank=True)  # Optional link field
 
     def __str__(self):
         return self.name
