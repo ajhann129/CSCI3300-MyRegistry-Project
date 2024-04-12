a4ec207d4456a17988ccb825a134d524cdf7b545
George <lwhittington2231@gmail.com>
2024-04-12 08:44:49 -0400

    Removed var declarations

--- registry/view_registry_app/static/view_registry_app/js/script.js
+++ registry/view_registry_app/static/view_registry_app/js/script.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     if (window.location.pathname === '/dashboard') {
         document.getElementById('dashboardForm').addEventListener('submit', function(event) {
             // Get the wishlist_id
-            var wishlistId = document.getElementById('wishlist_id').value;
+            let wishlistId = document.getElementById('wishlist_id').value;
             // Add wishlist_id to the form data
-            var formData = new FormData(this);
+            let formData = new FormData(this);
             formData.append('wishlist_id', wishlistId);
             // Set the updated form data
             event.formData = formData;
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Add event listener to the wishlist button
     document.getElementById("wishlistButton").addEventListener("click", function() {
-        var wishlistURL = this.getAttribute("data-wishlist-url");
+        let wishlistURL = this.getAttribute("data-wishlist-url");
         console.log("Wishlist URL:", wishlistURL); // Check if the URL is printed correctly
         if (wishlistURL) {
             window.location.href = wishlistURL;
