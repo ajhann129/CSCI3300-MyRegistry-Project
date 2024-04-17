d956f8c57b2ffd339181da3ad81ff792932e0583
George <lwhittington2231@gmail.com>
2024-04-17 16:00:40 -0400

    Create item button is now bigger

--- registry/view_registry_app/static/view_registry_app/js/script.js
+++ registry/view_registry_app/static/view_registry_app/js/script.js
@@ -9,6 +9,22 @@ document.addEventListener('DOMContentLoaded', function() {
             formData.append('wishlist_id', wishlistId);
             // Set the updated form data
             event.formData = formData;
+        });
+
+        // Trigger form submission when the window loads
+        document.getElementById('dashboardForm').submit();
+    }
+
+    // Add event listener to the form submission if the URL is on the dashboard
+    if (window.location.pathname === '/dashboard') {
+        document.getElementById('dashboardForm').addEventListener('submit', function(event) {
+            // Get the wishlist_id
+            let wishlistId = document.getElementById('wishlist_id').value;
+            // Add wishlist_id to the form data
+            let formData = new FormData(this);
+            formData.append('wishlist_id', wishlistId);
+            // Set the updated form data
+            event.formData = formData;
             // Close the modal after form submission
             $('#createItemModal').modal('hide');
             // Trigger form submission when the window loads
