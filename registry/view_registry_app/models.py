5df8cc07450da4d3f328b721db09b651916cd7e9
squarenic <rendblad3@gmail.com>
2024-04-17 15:36:16 -0400

    Item Sorting and Clickable Links
    - Sort items by price and name (a-z, z-a, hi-lo, lo-hi)
    - Clickable links
    - Indicators showing what it is sorted by

--- registry/view_registry_app/models.py
+++ registry/view_registry_app/models.py
@@ -11,8 +11,12 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True) # Optional description field
     link = models.URLField(blank=True)  # Optional link field
+
+    @property
+    def total(self):
+        return self.price * self.quantity
 
     def __str__(self):
         return self.name
