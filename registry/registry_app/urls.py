e6cc44275815c53fcf5477d8b9fb37439e91da76
George <lwhittington2231@gmail.com>
2024-04-18 17:11:21 -0400

    Refactored places saying Wishlist to Registries to reflect the name or project

--- registry/registry_app/urls.py
+++ registry/registry_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'registry_app'
 
 urlpatterns = [
-    path('wishlist/', views.wishlist_view, name='wishlist'),
+    path('', views.wishlist_view, name='wishlist'),
     path('create_wishlist/', views.create_wishlist, name='create_wishlist'),
     path('delete_wishlist/<int:wishlist_id>/', views.delete_wishlist, name='delete_wishlist'),
     path('load_wishlists/', views.load_wishlists, name='load_wishlists'),
